9617c0d713900639d9716e0c5c2a61ec640a2801
Gleb Malikov Villegas ^-^ <44012947+glebmavi@users.noreply.github.com>
2025-12-08 15:54:52 +0300

    init

---
+++ src/app.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+from gui.main_window import MainWindow
+
+from gui.theme_manager import ThemeManager
+
+def main():
+    app = QApplication(sys.argv)
+    
+    # Apply Theme
+    ThemeManager.apply_theme(app, "dark")
+    
+    window = MainWindow()
+    window.show()
+    
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
